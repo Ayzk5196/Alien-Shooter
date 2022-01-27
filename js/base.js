8e84ac75368958981fd7cdd35ee753b1bc1f6a68
Ayzk5196 <86311072+Ayzk5196@users.noreply.github.com>
2022-01-27 17:50:59 -0500

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,21 +1,23 @@
 class Base{
     constructor(){
-        this.player=createSprite(width/2,height-100,50,50);
+        this.image = loadImage("assets/Standingman1.png");
+        this.player=createSprite(100,height/2,50,50);
+        this.player.addImage(this.image);
         this.bg = bg;
         this.alienGroup = createGroup();
     }
     controlPlayer(){
-        if(keyDown('UP_ARROW')){
-            this.player.y -=15;
+        if(keyDown('UP_ARROW') && this.player.y>50){
+            this.player.y -=12;
         }
-        if(keyDown('DOWN_ARROW')){
-            this.player.y +=15;
+        if(keyDown('DOWN_ARROW') && this.player.y<height-50){
+            this.player.y +=12;
         }
-        if(keyDown('LEFT_ARROW')){
-            this.player.x -=15;
+        if(keyDown('LEFT_ARROW') && this.player.x>50){
+            this.player.x -=12;
         }
-        if(keyDown('RIGHT_ARROW')){
-            this.player.x +=15;
+        if(keyDown('RIGHT_ARROW') && this.player.x<width-50){
+            this.player.x +=12;
         }
     }
     show(){
@@ -27,9 +29,9 @@ class Base{
         drawSprites();
     }
     createAliens(){
-        var alien = createSprite(random(100,width-100),0,40,40);
-        alien.velocityY=5;
-        alien.lifetime=200;
+        var alien = createSprite(width+10,random(100,height-100),40,40);
+        alien.velocityX=-5;
+        alien.lifetime=400;
         this.alienGroup.add(alien);
     }
 }
